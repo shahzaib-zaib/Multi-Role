6520d5b0458b0467f8fe592f7d32e7905328a427
Shahzaib <shahzaibux@gmail.com>
2020-07-09 19:26:50 +0500

    Project Initiaied

--- RegisterationForm.php
+++ RegisterationForm.php
@@ -12,6 +12,31 @@
 
         <p>Enter Your Email</p>
         <input type="email" name="name" id="">
+
+        <p>Enter Your Password</p>
+        <input type="password" name="pswd" id="">
+
+        <p>Who are you?</p>
+        <select name="userole" id="">
+            <option value="customer">Customer</option>
+            <option value="seller">Seller</option>
+        </select>
     </form>
+
+    <?php
+
+        include "config.php";
+        if(isset($_POST['btn'])){
+            $a = $_POST['name'];
+            $b = $_POST['email'];
+            $c = $_POST['pswd'];
+            $d = $_POST['userrole'];
+
+            $insert_query = "INSERT INTO registrationtable(Name, Email, Password, Role)
+            VALUES ('$a', '$b', '$c', '$d')";
+
+        }
+
+    ?>
 </body>
 </html>
